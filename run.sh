30f324e08f001d7257d93e91a45a3454bf156e68
MeepCat55 <89799752+meepcat55@users.noreply.github.com>
2025-04-11 18:29:37 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sudo python3 kindle-build-env1.py
-distrobox create -i archlinux -n kindle-build-env -volume $(pwd)
+distrobox create -i archlinux -n kindle-build-env --home $(pwd)
 dir=$(pwd)/kindle-build-env2.py
 echo "sudo pacman -S python3" > python.sh
 echo "python3 $dir" >> python.sh
-distrobox enter kindle-build-env -- bash $(pwd)/python.sh
+distrobox enter kindle-build-env -- bash python.sh
