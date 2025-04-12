fc705e4a4d7f825479a66059242dff151bf2ae14
MeepCat55 <89799752+meepcat55@users.noreply.github.com>
2025-04-11 20:31:42 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ dir=$(pwd)/kindle-build-env2.py
 echo "sudo pacman -S python3" > python.sh
 echo "python3 $dir" >> python.sh
 distrobox enter kindle-build-env -- bash python.sh
+distrobox enter kindle-build-env
